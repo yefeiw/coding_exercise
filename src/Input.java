8422e3768e47eac51606521ba854c53abd900ab8
Yefei Wang <yefeiw@quicinc.com>
2017-03-13 16:01:49 -0700

    first test running

---
+++ src/Input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//package utils;
+public class Input {
+	//member functions to generate input.
+	public ArrayList<Integer> generateRandomArrayList() {
+		Random rand = new Random();
+		int maxSize = 10000;
+		int size = rand.nextInt(maxSize+1);
+		ArrayList<Integer> ret = new ArrayList<Integer>();//returning arraylist of integer.
+		for (int i = 0; i < size; i++) {
+			ret.add(rand.nextInt());
+		}
+		return ret;
+	}
+}
--- src/PreviousPermutation.java
+++ src/PreviousPermutation.java
@@ -1,38 +1,21 @@
 import java.util.*;
-private class Solution{
-    /**
-     * @param nums: A list of integers
-     * @return: A list of integers that's previous permuation
-     */
-    public ArrayList<Integer> previousPermuation(ArrayList<Integer> nums) {
-		// write your code
-		if (null == nums || nums.isEmpty()) {
-			return nums;
-		}
-        int inputSize = nums.size();
-        int i = inputSize -1;
-        for (i = inputSize -1; i >0 && nums.get(i) >= nums.get(i-1);i--);
-        swapList(nums,i,inputSize -1);
-        if (i != 0 ) {
-            int j = i;
-            while(nums.get(j) >= nums.get(i-1) ) {
-                j++;
-            }
-            swapItem(nums,j,i-1);
-        }
-        return nums;
-    }
-};
+//import Input.*;
+
 public class PreviousPermutation {
-    public ArrayList<Integer> input;
-    public ArrayList<Integer> output;
-    public ArrayList<Integer> ref;
-    int testIteration = 10000;
-    void main (String args[]) {
+
+    public static void main (String args[]) {
+        int testIteration = 10000;
+        ArrayList<Integer> input;
+        ArrayList<Integer> output;
+        ArrayList<Integer> ref;
+
+        Input  testInput = new Input();
+        Solution sol = new Solution();
         for (int i = 0; i < testIteration; i++) {
-            input = generateRandomArrayList();
-            output = PreviousPermuation.previousPermuation(input);
-            ref = Ref.previousPermuation(input);
+            input = testInput.generateRandomArrayList();
+            System.out.println("generated input size"+input.size());
+            output = sol.previousPermuation(input);
+            ref = sol.previousPermuation(input);
             if(output != ref) {
                 System.out.println("diff found, exiting");
                 return;
@@ -40,4 +23,36 @@ public class PreviousPermutation {
         }
         System.out.println("test passed, ready to submit");
     }
-}
+
+};
+class Solution{
+    private void swapList(ArrayList<Integer> nums, int start, int end) {
+        while (start < end) {
+            Collections.swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers that's previous permuation
+     */
+    public ArrayList<Integer> previousPermuation(ArrayList<Integer> nums) {
+        // write your code
+        if (null == nums || nums.isEmpty()) {
+            return nums;
+        }
+        int inputSize = nums.size();
+        int i = inputSize -1;
+        for (i = inputSize -1; i >0 && nums.get(i) >= nums.get(i-1);i--);
+            swapList(nums,i,inputSize -1);
+        if (i != 0 ) {
+            int j = i;
+            while(nums.get(j) >= nums.get(i-1) ) {
+                j++;
+            }
+            Collections.swap(nums,j,i-1);
+        }
+        return nums;
+    }
+};
