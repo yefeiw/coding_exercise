230ec57ac2cda69114a5cb13362f240fccdb4134
Yefei Wang <yefeiw@quicinc.com>
2017-03-13 11:53:33 -0700

    regular commit: previous permutation

--- src/PreviousPermutation.java
+++ src/PreviousPermutation.java
@@ -6,9 +6,19 @@ public class PreviousPermutation{
      */
     public ArrayList<Integer> previousPermuation(ArrayList<Integer> nums) {
 		// write your code
-		ArrayList<Integer> ret;//returning list of integers
 		if (null == nums || nums.isEmpty()) {
-			return ret;
+			return nums;
 		}
+        int inputSize = nums.size();
+        for (int i = inputSize -1; i >0 && nums.get(i) >= nums.get(i-1);i--);
+        swapList(nums,i,inputSize -1);
+        if (i != 0 ) {
+            int j = i;
+            while(nums.get(j) >= nums.get(i-1) ) {
+                j++;
+            }
+            swapItem(nums,j,i-1);
+        }
+        return nums;
     }
 };
