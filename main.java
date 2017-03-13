8ed9ef0860eebae211caf0febf645ae5e5c2b846
Yefei Wang <yefeiw@quicinc.com>
2017-03-13 14:15:32 -0700

    file struct changes

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int i = scan.nextInt();
--- src/PreviousPermutation.java
+++ src/PreviousPermutation.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class PreviousPermutation{
+private class Solution{
     /**
      * @param nums: A list of integers
      * @return: A list of integers that's previous permuation
@@ -10,7 +10,8 @@ public class PreviousPermutation{
 			return nums;
 		}
         int inputSize = nums.size();
-        for (int i = inputSize -1; i >0 && nums.get(i) >= nums.get(i-1);i--);
+        int i = inputSize -1;
+        for (i = inputSize -1; i >0 && nums.get(i) >= nums.get(i-1);i--);
         swapList(nums,i,inputSize -1);
         if (i != 0 ) {
             int j = i;
@@ -22,3 +23,21 @@ public class PreviousPermutation{
         return nums;
     }
 };
+public class PreviousPermutation {
+    public ArrayList<Integer> input;
+    public ArrayList<Integer> output;
+    public ArrayList<Integer> ref;
+    int testIteration = 10000;
+    void main (String args[]) {
+        for (int i = 0; i < testIteration; i++) {
+            input = generateRandomArrayList();
+            output = PreviousPermuation.previousPermuation(input);
+            ref = Ref.previousPermuation(input);
+            if(output != ref) {
+                System.out.println("diff found, exiting");
+                return;
+            }
+        }
+        System.out.println("test passed, ready to submit");
+    }
+}
