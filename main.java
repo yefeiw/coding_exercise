777b6f017785981c0280009a33a06e7a209a0930
Yefei Wang <yefeiw@quicinc.com>
2017-03-13 11:02:06 -0700

    skeleton code in main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = scan.nextInt();
+    }
+}
