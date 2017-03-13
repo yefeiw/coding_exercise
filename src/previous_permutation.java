a902ffa33565bc2be9fb564c3a48fac5fa02ee04
Yefei Wang <yefeiw@quicinc.com>
2017-03-13 11:32:32 -0700

    first checkin of previous permutation

---
+++ src/previous_permutation.java
@@ -0,0 +1,13 @@
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers that's previous permuation
+     */
+    public ArrayList<Integer> previousPermuation(ArrayList<Integer> nums) {
+		// write your code
+		ArrayList<Integer> ret;//returning list of integers
+		if (null == nums || nums.empty()) {
+			return ret;
+		}
+    }
+}
