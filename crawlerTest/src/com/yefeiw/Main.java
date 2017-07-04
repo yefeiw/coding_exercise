133913ac282fdeeaeba943e1326c8f80528e0814
Yefei Wang <yefeiw@Yefeis-MacBook-Pro.local>
2017-07-04 15:51:38 -0700

    ready to submit

--- crawlerTest/src/com/yefeiw/Main.java
+++ crawlerTest/src/com/yefeiw/Main.java
@@ -28,31 +28,38 @@ public class Main {
             while (starter != null) {
                 //read next valid input line
                 String input = queryHandler.getLine();
-                if(input.trim().length() == 0) {
-                    continue;
-                }
                 if (input == null) {
                     throw (new EOFException("EOF reached"));
                 }
+                if(input.length()== 0 || input.trim().length() == 0) {
+                    continue;
+                }
                 logger.info("input query is "+ input);
                 String[] words = input.split(",");
+                //let query handler clean up the keywords
+                words[0] = queryHandler.tokenizeString(words[0]);
                 //let the crawler process the ads
-                crawler.getAmazonProds(words,results,0);
+                crawler.getAmazonProds(words,results);
                 logger.info("total ads get from this page is "+results.size());
                 if(results.size() == 0) {
                     logger.warning("Input empty for keywords "+words[0].trim());
-                    throw(new EOFException("input empty"));
+                    continue;
                 }
                 //write output to files
                 try {
-                    objectMapper.writeValue(new File("output_" + fileNum++ + ".json"), results);
+                    objectMapper.writeValue(new File("out/output_" + fileNum++ + ".json"), results);
                 } catch (Exception e) {
                     logger.warning("File output error");
                     e.printStackTrace();
                 }
                 results = new ArrayList<Ad>(1000);
             }
+        }catch (EOFException e) {
+            //No need to process EOF
+            logger.info("Processing finished");
+            return;
         }catch (Exception e) {
+            logger.warning("Found exception in Main function");
             e.printStackTrace();
     }
     }
