cde3ba3491147d96ac557574591b21961af6d9fa
Yefei Wang <yefeiw@Yefeis-MacBook-Pro.local>
2017-10-23 18:52:15 -0700

    updates

---
+++ advanced/facebook/third/78.subsets.java
@@ -0,0 +1,39 @@
+/*
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets
+ *
+ * algorithms
+ * Medium (41.07%)
+ * Total Accepted:    180.1K
+ * Total Submissions: 438.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 
+ * Given a set of distinct integers, nums, return all possible subsets.
+ * 
+ * Note: The solution set must not contain duplicate subsets.
+ * 
+ * 
+ * For example,
+ * If nums = [1,2,3], a solution is:
+ * 
+ * 
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [1],
+ * ⁠ [2],
+ * ⁠ [1,2,3],
+ * ⁠ [1,3],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ []
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+    }
+}
