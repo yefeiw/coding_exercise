f39887f099cbcf2ae428d8c593c9fe139641647d
yefeiw <jeffxanthus@gmail.com>
2018-02-26 18:13:22 -0500

    updates

---
+++ advanced/google/first/166.fraction-to-recurring-decimal.java
@@ -0,0 +1,73 @@
+/*
+ * [166] Fraction to Recurring Decimal
+ *
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/description/
+ *
+ * algorithms
+ * Medium (17.97%)
+ * Total Accepted:    61K
+ * Total Submissions: 339.1K
+ * Testcase Example:  '1\n5'
+ *
+ * Given two integers representing the numerator and denominator of a fraction,
+ * return the fraction in string format.
+ *
+ * If the fractional part is repeating, enclose the repeating part in
+ * parentheses.
+ *
+ * For example,
+ *
+ * Given numerator = 1, denominator = 2, return "0.5".
+ * Given numerator = 2, denominator = 1, return "2".
+ * Given numerator = 2, denominator = 3, return "0.(6)".
+ *
+ *
+ *
+ * Credits:Special thanks to @Shangrila for adding this problem and creating
+ * all test cases.
+ */
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+
+        StringBuilder res = new StringBuilder();
+        //first question to tell: is it plus or minus
+        res.append(((numerator > 0)^(denominator > 0))? "-" : "");
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+
+        //integral part
+        res.append(num/den);
+        num %= den;
+        if (num == 0) {
+            //no fractional part
+            return res.toString();
+        }
+
+        //fractional part
+        res.append(".");
+        //key is the possible recurring number.
+        //value is where to insert the opening bracket.
+        Map<Long, Integer> map  = new HashMap();
+        map.put(num,res.length());
+        while(num !=  0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+            if (map.containsKey(num)) {
+                int index = map.get(num);
+                res.insert(index, "(");
+                res.append(")");
+                break;
+            } else {
+                map.put(num,res.length());
+            }
+        }
+
+        return res.toString();
+
+
+    }
+}
