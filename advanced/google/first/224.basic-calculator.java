96028e64e347e0ddde28c7ddf5eae17ef40778ac
Yefei Wang <yefeiw@Yefeis-MacBook-Pro.local>
2018-02-28 11:45:32 -0800

    updates

--- advanced/google/first/224.basic-calculator.java
+++ advanced/google/first/224.basic-calculator.java
@@ -30,6 +30,45 @@
  */
 class Solution {
     public int calculate(String s) {
-        
+       int ret = 0;
+       Deque<Integer> stack = new ArrayDeque();
+       //iterator
+       int pos = 0;
+       //current sign, 1 or -1
+       int sign = 1;
+       //current value to be returned;
+       int curVal = 0;
+       while(pos < s.length()) {
+        if (s.charAt(pos) == ' ') {
+            pos++;
+            continue;
+        } else if (Character.isDigit(s.charAt(pos))) {
+           int cur = s.charAt(pos) - '0';
+           //BUG: to greatly simplify the operations, we need to walk all the way into the digits till completion.
+           //Otherwise the code will be not readable and also bug prone
+           while(pos + 1 < s.length() && Character.isDigit(s.charAt(pos + 1))) {
+               cur = 10 * cur + s.charAt(++pos) - '0';
+           }
+           curVal += sign * cur;
+        } else if (s.charAt(pos)=='+' || s.charAt(pos) == '-') {
+            sign = (s.charAt(pos) == '-') ? -1 : 1;
+        } else if (s.charAt(pos) == '(') {
+            stack.push(curVal);
+            stack.push(sign);
+            curVal = 0;
+            sign = 1;
+        } else if (s.charAt(pos) ==  ')') {
+            int prevSign = stack.pop();
+            int prevValue = stack.pop();
+            curVal = prevValue + (prevSign) * curVal;
+            //BUG: here since we have finished bracket processing, we should mark the sign back to 1.
+            sign = 1;
+        } else {
+            //invalid characters, return 0;
+            return 0;
+        }
+        pos++;
+       }
+       return curVal;
     }
 }
