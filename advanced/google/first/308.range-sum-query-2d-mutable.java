05912fc7ec33a64199d4598d6161e6da88c87387
Yefei Wang <yefwang@yefwang-mn1.linkedin.biz>
2018-08-04 17:24:03 -0700

    BIT problem touched

--- advanced/google/first/308.range-sum-query-2d-mutable.java
+++ advanced/google/first/308.range-sum-query-2d-mutable.java
@@ -45,17 +45,56 @@
  * 
  */
 class NumMatrix {
+    /**
+     * The solutions uses binary index tree. 
+     */
+    //binary intex tree
+    int[][] tree;
+    int[][] nums;
+    int m, n;
 
     public NumMatrix(int[][] matrix) {
-        
+       if(matrix.length == 0 || matrix[0].length == 0) return;
+       m = matrix.length;
+       n = matrix[0].length;
+       tree = new int[m+1][n+1];
+       nums = new int[m][n];
+
+       for(int i = 0; i < m ; i++) {
+           for (int j = 0; j < n; j++) {
+               //Use the same function to both initialize and to udpate the tree.
+               update (i,j,matrix[i][j]);
+           }
+       }
     }
     
     public void update(int row, int col, int val) {
-        
+        if (m == 0 || n == 0) return;
+        int delta = val - nums[row][col];
+        nums[row][col] = val;
+        //update the tree. Here notice that we are incrementing by the BIT properties.
+        for (int i = row + 1; i <= m ; i += i & (-i)) {
+            for (int j = col + 1; j <= n; j += j & (-j)) {
+                tree[i][j] += delta;
+            }
+        }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        
+        if (m == 0 || n == 0) return 0;
+        //The following formula applies to all 2d partial sum
+        return sum(row2+1, col2+1) + sum (row1, col1) - sum (row1, col2+1) - sum (row2+1, col1);    
+    }
+
+    private int sum (int row, int col) {
+        int sum = 0;
+        //Below is also updating the tree using BIT property.
+        for (int i = row; i>0; i -=(i & (-i))) {
+            for (int j = col; j > 0; j -= (j & (-j))) {
+                sum += tree[i][j];
+            }
+        }
+        return sum;
     }
 }
 
